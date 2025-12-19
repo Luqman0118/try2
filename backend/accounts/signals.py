3e9091c4492fc5c77f5b0985332d73c3d84f2832
Luqman trisna permana <luqmantrisnap@gmail.com>
2025-12-19 22:27:11 +0700

    Delete backend directory

--- backend/accounts/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save
-from django.contrib.auth.models import User
-from django.dispatch import receiver
-from .models import MahasiswaProfile
-
-@receiver(post_save, sender=User)
-def create_mahasiswa_profile(sender, instance, created, **kwargs):
-    if created:
-        MahasiswaProfile.objects.create(user=instance)
